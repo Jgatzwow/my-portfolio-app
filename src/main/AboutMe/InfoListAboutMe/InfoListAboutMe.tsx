2d97139b5136be4d6b99d6a456c3dbee695cce03
Mykhailo <mihasokol1993@gmail.com>
2023-01-14 09:34:36 +0200

    fixed some styles

--- src/main/AboutMe/InfoListAboutMe/InfoListAboutMe.tsx
+++ src/main/AboutMe/InfoListAboutMe/InfoListAboutMe.tsx
@@ -12,10 +12,10 @@ type PropsType = {
 export const InfoListAboutMe: React.FC<PropsType> = ({ data }) => {
   const mappedListItem = data.map((row) => {
     return (
-      <div key={row.id} className={styles.infoList__item}>
+      <li key={row.id} className={styles.infoList__item}>
         <span className={styles.infoList__text}>{row.field}</span>
         <h6 className={styles.infoList__personal}>{row.data}</h6>
-      </div>
+      </li>
     );
   });
   return <ul className={styles.infoList}>{mappedListItem}</ul>;
--- src/main/Home.tsx
+++ src/main/Home.tsx
@@ -29,7 +29,7 @@ export const Home = () => {
           </div>
         </div>
         <div className={styles.photo__wrap}>
-          <img className={styles.photo} src={homePicture} alt="My picture" />
+          <img className={styles.photo} src={homePicture} />
         </div>
       </div>
     </div>
